e112f0f6289997c49140c2faace51f4c0ee070ec
Terence Hughes <Terencehughes450@gmail.com>
2024-01-31 18:43:32 -0500

    added button with span elements for song title, artist, and duration

--- index.js
+++ index.js
@@ -89,4 +89,13 @@ let userData = {
   };
 
   const renderSongs = (array) => {}
-  const songsHTML = array.map()
+  const songsHTML = array.map((song)=>{
+    return `<li id = "song-${song.id}" class = "playlist-song">
+    <button class="playlist-song-info">
+        <span class = "playlist-song-title">${song.title}</span>
+        <span class = "playlist-song-artist">${song.artist}</span>
+        <span class = "playlist-song-duration">${song.duration}</span>
+    </button>
+    </li>
+    `
+  })
