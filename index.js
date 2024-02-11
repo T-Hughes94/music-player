5af79036b0196675b4b55b600a5b2f5d0265f821
Terence Hughes <Terencehughes450@gmail.com>
2024-02-11 17:05:42 -0500

    added a variable called playPreviousSong and assigned it an empty arrow function

--- index.js
+++ index.js
@@ -109,14 +109,18 @@ const pauseSong = ()=>{
 };
 
 const playNextSong = () => {
+  
   if(userData?.currentSong === null){
     playSong(userData?.songs[0].id)
   }else{
     const currentSongIndex = getCurrentSongIndex()
     const nextSong = userData?.songs[currentSongIndex + 1]
+   
     playSong(nextSong.id)
   }
-}
+};
+
+const playPreviousSong = () => {};
 
 const renderSongs = (array) => {
   const songsHTML = array
