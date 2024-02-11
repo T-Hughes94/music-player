6a134cd20179fe207da44c43cdeba5ea4a0300a7
Terence Hughes <Terencehughes450@gmail.com>
2024-02-11 17:10:43 -0500

    finished playPreviousSong function

--- index.js
+++ index.js
@@ -120,7 +120,15 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {};
+const playPreviousSong = () => {
+  if(userData?.currentSong === null){
+    return
+  }else{
+    const currentSongIndex = getCurrentSongIndex()
+    const previousSong = userData?.songs[currentSongIndex - 1]
+    playSong(previousSong.id)
+  }
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
