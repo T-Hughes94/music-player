10ec85c150a87c802e37e98835fc7689e2be0ec7
Terence Hughes <Terencehughes450@gmail.com>
2024-02-11 17:13:12 -0500

    added an event listener to the previous button that listens for a click and then executes the playPreviousSong function

--- index.js
+++ index.js
@@ -168,6 +168,8 @@ pauseButton.addEventListener("click", pauseSong)
 
 nextButton.addEventListener("click", playNextSong)
 
+previousButton.addEventListener("click", playPreviousSong)
+
 userData?.songs.sort((a,b) => {
   if (a.title < b.title) {
     return -1;
