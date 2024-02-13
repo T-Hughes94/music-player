a449c0318e462f04dc2ad288ba55ce8aa77ae69f
Terence Hughes <Terencehughes450@gmail.com>
2024-02-13 12:52:36 -0500

    set attribute to playButton with aria-label as the first argument and and a ternary for play as the second

--- index.js
+++ index.js
@@ -177,7 +177,8 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 const setPlayButtonAccessibleText = ()=>{
-  const song = userData?.currentSong || userData?.songs[0]
+  const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play")
 }
 
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
