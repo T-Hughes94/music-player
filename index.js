54a2e9580f95011a8c922154fd3376aa0356b30a
Terence Hughes <Terencehughes450@gmail.com>
2024-02-13 13:56:27 -0500

    inside of deleteSong function implicitly return userData?.songs.filter((song)=>? song.id !== id)

--- index.js
+++ index.js
@@ -142,6 +142,11 @@ const shuffle = ()=>{
   setPlayerDisplay();
   setPlayButtonAccessibleText();
 }
+
+const deleteSong = (id)=>{
+  userData.songs = userData?.songs.filter((song)=> song.id !== id);
+};
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById('player-song-title')
   const songArtist = document.getElementById('player-song-artist')
